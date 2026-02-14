51d02359471310501d1413b6014c98ccf3d68125
ciulicDev <angelo.prospero.developer@gmail.com>
2026-02-14 13:22:57 +0100

    lettura csv

--- main.py
+++ main.py
@@ -7,3 +7,23 @@ input_file ="utenti_da_inviare.csv"
 api_url = "https://api.example.com/users"  # esempio API reale
 api_key = "LA_TUA_API_KEY"  # sostituisci con la tua chiave
 
+#header con autenticazione
+
+headers = {
+    "Authorization": f"Bearer {api_key}",
+    "Content-Type": "application/json"
+}
+
+utenti = []
+
+
+#lettura csv 
+
+with open(input_file, newline="") as f:
+    lettore = csv.DictReader(f)
+    for riga in lettore:
+        utenti.append({
+            "name":riga["name"],
+            "username":riga["username"],
+            "email": riga["email"]
+        })
