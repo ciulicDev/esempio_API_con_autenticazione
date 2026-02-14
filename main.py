9fc2421f78053663e21e398742f8e67dd16229ad
ciulicDev <angelo.prospero.developer@gmail.com>
2026-02-14 13:19:42 +0100

    configurazione per la chiamata api

---
+++ main.py
@@ -0,0 +1,9 @@
+import csv 
+import json
+import requests
+
+#config
+input_file ="utenti_da_inviare.csv"
+api_url = "https://api.example.com/users"  # esempio API reale
+api_key = "LA_TUA_API_KEY"  # sostituisci con la tua chiave
+
