d12c93cffc55b7d059a071b37ca4a2bd4bdfbec3
ciulicDev <angelo.prospero.developer@gmail.com>
2026-02-14 13:47:15 +0100

    invio del post con headers di autenticazione

--- main.py
+++ main.py
@@ -27,3 +27,26 @@ with open(input_file, newline="") as f:
             "username":riga["username"],
             "email": riga["email"]
         })
+
+risposte = []
+
+#invio post con headers di autenticazione 
+
+for u in utenti:
+    response = requests.post(api_url, json=u, headers=headers)
+    if response.status_code in [200,201]:
+        risposte.append(response.json())
+        print(f"{u["name"]} salvato con successo")
+    elif response.status_code == 401:
+        print(f"Errore di autenticazione per {u["name"]} (401)")
+        risposte.append({
+            "name":u["name"],
+            "error": "Unauthorized"
+        })
+    
+    else:
+        print(f"Errore per {u["name"]}: {response.status_code}")
+        risposte.append({
+            "name": u["name"],
+            "Error": response.status_code
+        })
